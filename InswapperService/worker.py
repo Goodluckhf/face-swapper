3094248c106965b3115b4b244e72a42f1d820e43
Konstantin Bubyakin <goodluckhf@yandex.ru>
2023-12-28 20:00:15 +0600

    chore: timing

--- InswapperService/worker.py
+++ InswapperService/worker.py
@@ -11,6 +11,7 @@ import io
 import cv2
 import numpy as np
 import time
+from timeit import default_timer as timer
 
 celery = Celery(__name__)
 celery.conf.broker_url = os.environ.get("CELERY_BROKER_URL", "redis://127.0.0.1:6379")
@@ -18,6 +19,8 @@ celery.conf.result_backend = os.environ.get("CELERY_RESULT_BACKEND", "redis://12
 
 @celery.task(bind=True, base=Models)
 def process_image(self, source_file, target: str):
+    startFaceswap = timer()
+
     source_img = Image.open(source_file)
     os.remove(source_file)
 
@@ -25,22 +28,27 @@ def process_image(self, source_file, target: str):
         target_response = ObjectStorage.get_file(target)
     except:
         raise TargetException()
-    
+
     target_img = Image.open(target_response)
     target_response.close()
     target_response.release_conn()
 
     result_image = process([source_img], target_img, '0', '-1', self.models)
+    endFaceswap = timer()
+    print("face swap end: ",  endFaceswap - startFaceswap)
+    startScale = timer()
     upsampler = set_realesrgan()
     codeformer_net = ARCH_REGISTRY.get("CodeFormer")(dim_embd=512, codebook_size=1024, n_head=8, n_layers=9, connect_list=["32", "64", "128", "256"]).to(self.device)
     codeformer_net.load_state_dict(self.checkpoint)
     codeformer_net.eval()
     result_image = cv2.cvtColor(np.array(result_image), cv2.COLOR_RGB2BGR)
-    result_image = face_restoration(result_image, True, True, True, 0.5, upsampler, codeformer_net, self.device)
+    result_image = face_restoration(result_image, False, False, False, 0.5, upsampler, codeformer_net, self.device)
     result_image = Image.fromarray(result_image)
     img_bytes = io.BytesIO()
     result_image.save(img_bytes, format='PNG')
     img_bytes.seek(0)
+    endScale = timer()
+    print("end scale: ",  endScale - startScale)
 
     name = f"result_{uuid.uuid4()}.png"
     ObjectStorage.save_file(name, img_bytes)
