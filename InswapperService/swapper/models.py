e7abc8014d1ec98f5ed1b760c98156c6e4823ef7
Toshi711 <romakozhevnik@mail.ru>
2023-12-28 17:11:52 +0400

    Code former

--- InswapperService/swapper/models.py
+++ InswapperService/swapper/models.py
@@ -6,7 +6,9 @@ import onnxruntime
 class Models(Task):
     def __init__(self):
         super().__init__()
+        self.ckpt_path = "CodeFormer/CodeFormer/weights/CodeFormer/codeformer.pth"
         signals.worker_init.connect(self.init_models)
+        signals.worker_init.connect(self.init_device)
 
     def init_models(self, *args, **kwargs):
         providers = onnxruntime.get_available_providers()
@@ -18,6 +20,13 @@ class Models(Task):
         self.model = model
         self.face_analyser = face_analyser
     
+    def init_device(self, *args, **kwargs):
+        check_ckpts()
+        device = torch.device("mps" if torch.backends.mps.is_available() else "cuda" if torch.cuda.is_available() else "cpu")
+        checkpoint = torch.load(self.ckpt_path)["params_ema"]
+        self.checkpoint = checkpoint
+        self.device = device
+
     @property
     def models(self):
         return (self.model, self.face_analyser)
