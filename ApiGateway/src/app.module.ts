c6ba771a8683664be20ae177ab6e4c3eac79c7de
Konstantin Bubyakin <goodluckhf@yandex.ru>
2024-01-03 21:21:42 +0600

    remove unused code

--- ApiGateway/src/app.module.ts
+++ ApiGateway/src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { FaceSwapperModule } from './face-swapper/face-swapper.module';
 
 @Module({
   imports: [ConfigModule.forRoot({ isGlobal: true }), FaceSwapperModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
