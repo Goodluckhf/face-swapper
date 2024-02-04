f24004e6f81f806d4c19a5c9894d60b1a9567517
Konstantin Bubyakin <goodluckhf@yandex.ru>
2024-02-04 15:42:07 +0600

    feat(): save baseImage settings

--- ApiGateway/src/face-swapper/users/users.models.ts
+++ ApiGateway/src/face-swapper/users/users.models.ts
@@ -1,7 +1,10 @@
+import { BasePhoto } from '../../Schemas/Image.schema';
+
 export interface Limit {
   limit: number;
   extraGenerationAvailable?: boolean;
   result?: string;
+  basePhoto?: BasePhoto;
   textphoto?: string;
   textcaption?: string;
 }
